0a8e9045e81850ed9b9f6b2779db05bae645d216
Henrik Lurz <henrik.lurz@web.de>
2023-10-18 22:49:07 +0200

    Created match empty world launch file specific for this world using the general gazebo world spawn

--- dt_gazebo/launch/match_empty_world.launch.py
+++ dt_gazebo/launch/match_empty_world.launch.py
@@ -3,17 +3,13 @@ from launch import LaunchDescription
 
 from launch.actions import (
     IncludeLaunchDescription,
-    DeclareLaunchArgument,
 )
 
 from launch.substitutions import (
     PathJoinSubstitution,
-    PythonExpression,
-    LaunchConfiguration,
 )
 
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node, SetParameter
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
@@ -41,59 +37,3 @@ def generate_launch_description():
             gz_sim
         ]
     )
-
-    # # world_cfg = LaunchConfiguration("world")
-    # # declare_world_arg = DeclareLaunchArgument(
-    # #     "world", default_value=world_file, description="match empty world file"
-    # # )
-
-    # headless = LaunchConfiguration("headless")
-    # declare_headless_arg = DeclareLaunchArgument(
-    #     "headless",
-    #     default_value="False",
-    #     description=("Run Gazebo Ignition in the headless mode"),
-    # )
-
-    # headless_cfg = PythonExpression(
-    #     [
-    #         "'--headless-rendering -s -r' if ",
-    #         headless,
-    #         " else '-r'",
-    #     ]
-    # )
-    # gz_args = [headless_cfg, " ", world_file]
-
-    # use_gpu = LaunchConfiguration("use_gpu")
-    # declare_use_gpu_arg = DeclareLaunchArgument(
-    #     "use_gpu",
-    #     default_value="True",
-    #     description="Whether GPU acceleration is used",
-    # )
-
-    # gz_sim = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource(
-    #         PathJoinSubstitution(
-    #             [
-    #                 get_package_share_directory("ros_gz_sim"),
-    #                 "launch",
-    #                 "gz_sim.launch.py",
-    #             ]
-    #         )
-    #     ),
-    #     launch_arguments={
-    #         "gz_args": gz_args,
-    #         "on_exit_shutdown": "True",
-    #     }.items(),
-    # )
-
-    # return LaunchDescription(
-    #     [
-    #         # declare_world_arg,
-    #         declare_headless_arg,
-    #         declare_use_gpu_arg,
-    #         # Sets use_sim_time for all nodes started below
-    #         # (doesn't work for nodes started from ignition gazebo)
-    #         SetParameter(name="use_sim_time", value=True),
-    #         gz_sim,
-    #     ]
-    # )
