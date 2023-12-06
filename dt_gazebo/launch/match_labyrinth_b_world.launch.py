94b80c79da6bea4b8d52921e9feb768356e8e741
Henrik Lurz <henrik.lurz@web.de>
2023-12-06 15:36:57 +0100

    Added new maps for task 4 and milestone 4

---
+++ dt_gazebo/launch/match_labyrinth_b_world.launch.py
@@ -0,0 +1,39 @@
+
+from launch import LaunchDescription
+
+from launch.actions import (
+    IncludeLaunchDescription,
+)
+
+from launch.substitutions import (
+    PathJoinSubstitution,
+)
+
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    world_file = PathJoinSubstitution([get_package_share_directory("dt_gazebo"), 
+                                       "worlds", 
+                                       "match_labyrinth_b_world.sdf"])
+    
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [
+                    get_package_share_directory("dt_gazebo"),
+                    "launch",
+                    "gazebo_world.launch.py",
+                ]
+            )
+        ),
+        launch_arguments={
+            "world": world_file,
+        }.items(),
+    )
+
+    return LaunchDescription(
+        [
+            gz_sim
+        ]
+    )
